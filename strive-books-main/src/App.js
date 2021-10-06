194b0f3f4c0a60b52396e86ec9221b434254eb8d
Rafael Ferreira <rafaelnevesf@gmail.com>
2021-10-06 08:17:06 +0200

    Change selected

--- strive-books-main/src/App.js
+++ strive-books-main/src/App.js
@@ -5,25 +5,15 @@ import MyBadge from './components/MyBadge'
 import SingleBook from './components/SingleBook'
 import BookList from './components/BookList'
 import fantasyBooks from './fantasyBooks.json'
-import {Container, Row, Col} from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Container>
-          <Row>
-            <Col className="col-6">
-              {/* <WarningSign text="Watch out again!" /> */}
-              {/* <MyBadge text="NEW!!" color="info" /> */}
-              {/* <SingleBook book={fantasyBooks[0]} /> */}
-              <BookList books={fantasyBooks} />
-            </Col>
-            <Col className="col-6">
-              
-            </Col>
-          </Row>
-        </Container>
+        {/* <WarningSign text="Watch out again!" /> */}
+        {/* <MyBadge text="NEW!!" color="info" /> */}
+        {/* <SingleBook book={fantasyBooks[0]} /> */}
+        <BookList books={fantasyBooks} />
       </header>
     </div>
   )
--- strive-books-main/src/components/SingleBook.jsx
+++ strive-books-main/src/components/SingleBook.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import BookList from './BookList'
 import CommentArea from './CommentArea'
 
 class SingleBook extends React.Component {
@@ -12,17 +13,18 @@ class SingleBook extends React.Component {
         return (
             <>
                 <Card
-                    onClick={() => this.setState({ selected: !this.state.selected })}
-                    style={{ border: this.state.selected ? '3px solid red' : 'none' }}
+                    onClick={() => this.props.changeSelectedBook(this.props.book.asin)}
+                    /* onClick={() => this.setState({ selected: !this.state.selected })} */
+                    /* style={{ border: this.state.selected ? '3px solid red' : 'none' }} */
                 >
                     <Card.Img variant="top" src={this.props.book.img} />
                     <Card.Body>
                         <Card.Title style={{ color: 'black' }}>{this.props.book.title}</Card.Title>
                     </Card.Body>
                 </Card>
-                {
+                {/* {
                     this.state.selected && <CommentArea asin={this.props.book.asin} />
-                }
+                } */}
             </>
         )
     }
